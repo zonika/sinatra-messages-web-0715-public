e171d88ad4e82a96fcf22a66bb3bb17645359b8e
Logan Hasson <logan.hasson@gmail.com>
2014-06-30 14:41:36 -0400

    Add AR connection to environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'db/sinatra_messages.sqlite3'
+)
+
 require_relative '../models/message'
 require_relative '../app'
